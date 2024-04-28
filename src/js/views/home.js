94e341ab703ecaec9417eb4031dc6d88cc682219
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-04-28 18:54:52 +0000

    small refinements, property preview still does not work

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -16,40 +16,41 @@ export const Home = () => {
     return (
         <div className="container mt-5">
             <div className="card-container d-flex flex-nowrap overflow-auto">
-    {store.characters &&
-        store.characters.map((el) => {
-            const { height, mass, hair_color, name } = el;
-            console.log("Height:", height);
-            console.log("Mass:", mass);
-            console.log("Hair Color:", hair_color);
-            console.log("Name:", name);
-            return (
-                <div key={el.uid} className="card mx-3" style={{ minWidth: "300px" }}>
-                    <img
-                        src={`https://starwars-visualguide.com/assets/img/characters/${el.uid}.jpg`}
-                        className="card-img-top"
-                        alt={el.name}
-                    />
-                    <div className="card-body d-flex justify-content-between align-items-center">
-                        <div>
-                            <h5 className="card-title">{el.name}</h5>
-                            <p>Height: {el.height}</p>
-                            <p>Mass: {el.mass}</p>
-                            <p>Hair Color: {el.hair_color}</p>
-                            <Link to={`/single/${el.uid}`} className="btn btn-primary">
-                                Learn more!
-                            </Link>
-                        </div>
-                        <div>
-                            <button className="btn btn-outline-danger" onClick={() => addToFavorites(el)}>
-                                <FontAwesomeIcon icon={faHeart} />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            );
-        })}
-</div>
+                {store.characters &&
+                    store.characters.map((el) => {
+                        const { height, mass, hair_color, name } = el;
+                        console.log("Height:", height);
+                        console.log("Mass:", mass);
+                        console.log("Hair Color:", hair_color);
+                        console.log("Name:", name);
+                        console.log(el);
+                        return (
+                            <div key={el.uid} className="card mx-3" style={{ minWidth: "300px" }}>
+                                <img
+                                    src={`https://starwars-visualguide.com/assets/img/characters/${el.uid}.jpg`}
+                                    className="card-img-top"
+                                    alt={el.name}
+                                />
+                                <div className="card-body d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <h5 className="card-title">{el.name}</h5>
+                                        <p>Height: {el.height}</p>
+                                        <p>Mass: {el.mass}</p>
+                                        <p>Hair Color: {el.hair_color}</p>
+                                        <Link to={`/single/${el.uid}`} className="btn btn-primary">
+                                            Learn more!
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <button className="btn btn-outline-danger" onClick={() => addToFavorites(el)}>
+                                            <FontAwesomeIcon icon={faHeart} />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
             <div className="card-container d-flex flex-nowrap overflow-auto">
                 {store.planets &&
                     store.planets.map((el) => (
